0c5251717da37a31b6f5ad2d1f20004247d86ac7
Nick Sullivan <nicholassull@gmail.com>
2021-11-04 11:48:28 -0700

    add const declairation for first question

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $("form#question-form").submit(function(event) {
+    const a1 = $("input:radio[name=question1-radios]:checked").val()
+
     event.preventDefault();
   });
 });
