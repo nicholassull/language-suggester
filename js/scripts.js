1845128a995c7cf092284cfbb70b50fd30cc93ec
Nick Sullivan <nicholassull@gmail.com>
2021-11-04 11:51:02 -0700

    add const declairation for remaining questsions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("form#question-form").submit(function(event) {
     const a1 = $("input:radio[name=question1-radios]:checked").val()
+    const a2 = $("input:radio[name=question2-radios]:checked").val()
+    const a3 = $("input:radio[name=question3-radios]:checked").val()
+    const a4 = $("input:radio[name=question4-radios]:checked").val()
+    const a5 = $("input:radio[name=question5-radios]:checked").val()
 
     event.preventDefault();
   });
