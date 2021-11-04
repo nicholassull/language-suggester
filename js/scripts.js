09187b1599d5acce87e4493bae9c73703cd0d15e
Nick Sullivan <nicholassull@gmail.com>
2021-11-04 11:56:37 -0700

    add parseInt() to const declarations

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   $("form#question-form").submit(function(event) {
-    const a1 = $("input:radio[name=question1-radios]:checked").val()
-    const a2 = $("input:radio[name=question2-radios]:checked").val()
-    const a3 = $("input:radio[name=question3-radios]:checked").val()
-    const a4 = $("input:radio[name=question4-radios]:checked").val()
-    const a5 = $("input:radio[name=question5-radios]:checked").val()
+    const a1 = parseInt($("input:radio[name=question1-radios]:checked").val());
+    const a2 = parseInt($("input:radio[name=question2-radios]:checked").val());
+    const a3 = parseInt($("input:radio[name=question3-radios]:checked").val());
+    const a4 = parseInt($("input:radio[name=question4-radios]:checked").val());
+    const a5 = parseInt($("input:radio[name=question5-radios]:checked").val());
+
+    const total = a1 + a2 + a3 + a4 + a5;
+    console.log(total);
 
     event.preventDefault();
   });
